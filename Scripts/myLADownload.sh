350a35f9ae335b41f57be95224a4e1a995ccadf2
L3MON4D3 <Simljk@outlook.de>
2020-05-05 12:12:58 +0200

    Change Download Script

--- Scripts/myLADownload.sh
+++ Scripts/myLADownload.sh
@@ -1,25 +1,10 @@
 #!/bin/bash
 cd $la
-COOKIE=cookie
-myCreateCookieECampus.sh "$COOKIE"
+myECampusDownload.sh "https://ecampus.uni-bonn.de/goto_ecampus_fold_1663264.html"
 
-function curlLogin() {
-    curl "$1" --cookie "$3" -o $2
-}
+for i in *.pdf; do
+    mkdir $(echo "$i" | perl -lpe's/.*?(\d{2})\.pdf/ex\/$1/g')
+done
 
-function downloadFilesFromEcampus(){
-    curlLogin "$1" temp "$4"
-    cat temp | 
-        \grep \<h4 | 
-        \perl -lpe"s/.*?a href=\"(.*?)\".*?>(.*?)<.*/\$1\n\$2/g" > $3.list
-    exCount=$(($(cat $3.list | wc -l)/2))
-    for ((i=1;i<=exCount;i++)); do
-        urlLine=$(($i*2-1))
-        nameLine=$(($urlLine+1))
-        url=$(sed "${urlLine}q;d" $3.list)
-        name=$(sed "${nameLine}q;d" $3.list)
-        curlLogin $url ./$3/$name "$COOKIE"
-    done
-}
-downloadFilesFromEcampus "https://ecampus.uni-bonn.de/goto_ecampus_fold_1663264.html" "\<h4" ex "$COOKIE"
-rm cookie ex.list temp
+perl-rename 's/Zettel(\d{2})\.pdf/ex\/$1\/Aufgabe/g' *.pdf
+perl-rename 's/Zettel(\d{2})_Musterloesung\.(.*)/ex\/$1\/Loesung\.$2/g' *
