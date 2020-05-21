39bb4a75cd34008bfe3318c5c9bbd4298f62ef9c
L3MON4D3 <Simljk@outlook.de>
2020-05-21 17:10:05 +0200

    remove temp files.

--- Scripts/myAnaDownload.sh
+++ Scripts/myAnaDownload.sh
@@ -5,3 +5,4 @@ URLDIR=${DIR/\ /%20}
 echo "https://uni-bonn.sciebo.de/s/hR8j6ZrjkCFnZyQ/download?path=${URLDIR}&files=" 
 wget "https://uni-bonn.sciebo.de/s/hR8j6ZrjkCFnZyQ/download?path=${URLDIR}&files=" -O .temp
 unzip .temp -d "$ana"
+rm .temp
