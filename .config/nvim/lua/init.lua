89995ee1f8b317f27f688216f9e8600c86117939
L3MON4D3 <Simljk@outlook.de>
2021-08-31 19:10:46 +0200

    Nicer keybinds for ts-textobjects.

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -195,17 +195,17 @@ require'nvim-treesitter.configs'.setup {
 				-- You can use the capture groups defined in textobjects.scm
 				["af"] = "@function.outer",
 				["if"] = "@function.inner",
-				["ac"] = "@class.outer",
-				["ic"] = "@class.inner",
+				["ac"] = "@call.outer",
+				["ic"] = "@call.inner",
 			},
 		},
 		swap = {
 			enable = true,
 			swap_next = {
-				["<leader>a"] = "@parameter.inner",
+				["<leader>>"] = "@parameter.inner",
 			},
 			swap_previous = {
-				["<leader>A"] = "@parameter.inner",
+				["<leader><"] = "@parameter.inner",
 			},
 		},
 		move = {
