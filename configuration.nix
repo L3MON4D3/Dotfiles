1f4002ab60e3c7747f75c0b2010856861f117073
Simon Katz <simon@l3mon4.de>
2025-01-16 08:58:49 +0100

    remove old code.

--- configuration.nix
+++ configuration.nix
@@ -13,21 +13,6 @@
     "flakes"
   ];
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  # };
-
-  # Enable the X11 windowing system.
-  # services.xserver.enable = true;
-
   # stable neovim for root user.
   programs.neovim = {
     enable = true;
