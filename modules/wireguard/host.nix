78b3fe0dd899f658cfa700c5d8a8d3cf40a37388
Simon Katz <simon@l3mon4.de>
2025-01-17 08:39:54 +0100

    slightly restructure wg-network-data.

--- modules/wireguard/host.nix
+++ modules/wireguard/host.nix
@@ -1,26 +1,25 @@
 wg_network: { config, lib, pkgs, machine, data, ... }:
 
 let
-  machine_conf = wg_network."${machine}";
+  machine_conf = wg_network.host;
   peernames = builtins.filter (x:
     x != "dns" &&
-    x != "endpoint" &&
-    x != "ifname" &&
-    x != machine ) (builtins.attrNames wg_network);
+    x != "name" &&
+    x != "host") (builtins.attrNames wg_network);
 in 
 {
   networking.nat.enable = true;
   networking.wg-quick.interfaces = {
-    "${wg_network.ifname}" = {
+    "${wg_network.name}" = {
       address = [machine_conf.address];
-      listenPort = lib.strings.toInt (builtins.elemAt (builtins.split ":" wg_network.endpoint) 2);
+      listenPort = lib.strings.toInt (builtins.elemAt (builtins.split ":" machine_conf.endpoint) 2);
       privateKeyFile = machine_conf.privkey_file;
       postUp = ''
-        ${pkgs.iptables}/bin/iptables -A FORWARD -i ${wg_network.ifname} -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -A FORWARD -i ${wg_network.name} -j ACCEPT
         ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s ${machine_conf.address} -o ${data.network.lan."${machine}".interface} -j MASQUERADE
       '';
       preDown = ''
-        ${pkgs.iptables}/bin/iptables -D FORWARD -i ${wg_network.ifname} -j ACCEPT
+        ${pkgs.iptables}/bin/iptables -D FORWARD -i ${wg_network.name} -j ACCEPT
         ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s ${machine_conf.address} -o ${data.network.lan."${machine}".interface} -j MASQUERADE
       '';
       peers = builtins.map (peername: let peerconf = wg_network."${peername}"; in {
