d8c0d8f20abafd25c6a924bf4a3efcb05a9dcb05
Simon Katz <simon@l3mon4.de>
2025-01-17 09:02:34 +0100

    add module for qbittorrent in netns.

--- machines/indigo/default.nix
+++ machines/indigo/default.nix
@@ -7,6 +7,7 @@
       ../../modules/localnet.nix
       (import ../../modules/wireguard/host.nix data.network.wireguard_home)
       (import ../../modules/wireguard/netns.nix data.network.wireguard_mullvad_de)
+      ../../modules/qbittorrent.nix
     ];
 
   boot.loader.systemd-boot.enable = true;
