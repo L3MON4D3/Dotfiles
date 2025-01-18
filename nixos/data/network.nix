31f4e0ef42f1de087274ec6cf818d293da6325ad
Simon Katz <simon@l3mon4.de>
2025-01-18 10:40:10 +0100

    make keepalive network-level setting, change network-names.
    wg_home is vpn with cinnabar as host, wg_home2 hosted by indigo.

--- nixos/data/network.nix
+++ nixos/data/network.nix
@@ -12,20 +12,35 @@
     gateway = "192.168.178.1";
     address_range = "192.168.178.0/24";
   };
-  wireguard_home = {
-    name = "wg_home";
+  wireguard_home2 = {
+    name = "wg_home2";
     host = {
       # indigo
       endpoint = "l3mon4.de:51822";
       address = "10.0.0.1/24";
-      privkey_file = "/var/secrets/wireguard_home/indigo_private";
+      privkey_file = "/var/secrets/wireguard_home2/indigo_private";
       pubkey = "UjwW2Cd7RbHlS+E7iKq1Igez+325QyxYm7wu3HseCRo=";
     };
     teal = {
       address = "10.0.0.2/24";
-      privkey_file = "/var/secrets/wireguard_home/teal_private";
+      privkey_file = "/var/secrets/wireguard_home2/teal_private";
       pubkey = "zJsfvvuFCh5AK2/wl4TZZZlWz2l4SM9fEdWy2mea/0c=";
     };
+    dns = "10.0.0.1";
+  };
+  wireguard_home = {
+    name = "wg_home";
+    host = {
+      # cinnabar
+      endpoint = "l3mon4.de:51822";
+      pubkey = "crcxsmvCRMSSY+ixB5p07MzxSv0xDmHPHBmd2w7mNyg=";
+    };
+    indigo = {
+      address = "10.0.0.7/24";
+      privkey_file = "/var/secrets/wireguard_home/indigo_private";
+      pubkey = "ln4vj6cBDF5FuJlqpUpNOq5nubesA7bR0HQE3PV2/DI=";
+    };
+    keepalive = true;
     dns = "10.0.0.1";
   };
   wireguard_mullvad_de = {
@@ -35,12 +50,12 @@
       privkey_file = "/var/secrets/wireguard_mullvad/de_unique_worm.private";
       pubkey = "9ldhvN7r4xGZkGehbsNfYb5tpyTJ5KBb5B3TbxCwklw=";
       local_address = "192.168.178.40/24";
-      keepalive = false;
     };
     host = {
       endpoint = "146.70.117.34:51820";
       pubkey = "9ldhvN7r4xGZkGehbsNfYb5tpyTJ5KBb5B3TbxCwklw="; 
     };
+    keepalive = false;
     dns = "10.64.0.1";
   };
 }
--- nixos/machines/indigo/default.nix
+++ nixos/machines/indigo/default.nix
@@ -5,8 +5,9 @@
     [
       ./hardware-configuration.nix
       ../../modules/localnet.nix
-      (import ../../modules/wireguard/host.nix data.network.wireguard_home)
+      (import ../../modules/wireguard/host.nix data.network.wireguard_home2)
       (import ../../modules/wireguard/netns.nix data.network.wireguard_mullvad_de)
+      (import ../../modules/wireguard/netns.nix data.network.wireguard_home)
       ../../modules/qbittorrent.nix
     ];
 
