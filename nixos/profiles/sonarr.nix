1d80dded1804474fee6efd893672ea057e692399
Simon Katz <simon@l3mon4.de>
2025-01-19 18:20:17 +0100

    add profile for sonarr.

---
+++ nixos/profiles/sonarr.nix
@@ -0,0 +1,85 @@
+{ config, lib, pkgs, machine, data, ... }:
+
+with lib;
+let
+  statedir = "/var/lib/sonarr";
+  wg_network = data.network.wireguard_mullvad_de;
+  wg_machine_conf = wg_network."${machine}";
+  port = wg_machine_conf.local_service_ports.sonarr;
+  conf = pkgs.writeTextFile {
+    name = "conf";
+    # $SONARR_APIKEY is provided via envsubst.
+    text = ''
+      <Config>
+        <BindAddress>*</BindAddress>
+        <Port>${port}</Port>
+        <SslPort>9898</SslPort>
+        <EnableSsl>False</EnableSsl>
+        <LaunchBrowser>False</LaunchBrowser>
+        <AuthenticationMethod>Forms</AuthenticationMethod>
+        <Branch>main</Branch>
+        <LogLevel>info</LogLevel>
+        <SslCertPath></SslCertPath>
+        <SslCertPassword></SslCertPassword>
+        <UrlBase></UrlBase>
+        <InstanceName>Sonarr</InstanceName>
+        <UpdateMechanism>External</UpdateMechanism>
+        <AuthenticationRequired>DisabledForLocalAddresses</AuthenticationRequired>
+        <ApiKey>''${SONARR_API_KEY}</ApiKey>
+      </Config>
+    '';
+  };
+in
+{
+  config = {
+    # reset settings to default, insert api-key.
+    system.activationScripts = {
+      sonarr = {
+        text = ''
+          install -d -o media -g media ${statedir}
+          source /var/secrets/sonarr_env
+          SONARR_API_KEY=$SONARR_API_KEY ${pkgs.envsubst}/bin/envsubst -i ${conf} -o ${statedir}/config.xml
+          chown media:media ${statedir}/config.xml
+        '';
+      };
+    };
+
+    # for now, until sonarr is updated to .Net 8, then these can be removed.
+    nixpkgs.config.permittedInsecurePackages = [
+      "aspnetcore-runtime-6.0.36"
+      "aspnetcore-runtime-wrapped-6.0.36"
+      "dotnet-sdk-6.0.428"
+      "dotnet-sdk-wrapped-6.0.428"
+    ];
+
+    systemd.services.sonarr_de = config.l3mon.network_namespaces.mkNetnsService wg_network {
+      enable = true;
+      description = "Run sonarr in network namespace de";
+      wantedBy = ["multi-user.target"];
+      serviceConfig = {
+        Type = "exec";
+      };
+      serviceConfig = {
+        User="media";
+        Group="media";
+      };
+      script = ''
+        ${pkgs.sonarr}/bin/Sonarr -data=${statedir}
+      '';
+    };
+    systemd.tmpfiles.rules = [
+      "d ${statedir} 0755 media media"
+    ];
+    
+    services.nginx.virtualHosts.sonarr = {
+      serverName = "sonarr sonarr.${machine}";
+      locations = {
+        "/" = {
+          proxyPass = "http://${wg_machine_conf.local_address}:${port}";
+          recommendedProxySettings = true;
+          proxyWebsockets = true;
+        };
+      };
+    };
+  };
+}
