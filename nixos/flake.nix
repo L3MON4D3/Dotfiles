9fa6c92a8ec55a431e8fdaa6cbcc878b0fbe2771
Simon Katz <simon@l3mon4.de>
2025-01-23 19:26:40 +0100

    nix: move home-manager config into own directory.

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -25,7 +25,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
 
-          home-manager.users.simon = import ./home.nix;
+          home-manager.users.simon = import ./home;
           home-manager.extraSpecialArgs = { inputs = inputs; };
         }
         {
