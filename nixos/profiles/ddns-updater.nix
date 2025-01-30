179c5f95caa7b517e07141dd42bc2a1c71e89b87
Simon Katz <simon@l3mon4.de>
2025-01-30 13:29:07 +0100

    nixos: add profile for ddns-updater.

---
+++ nixos/profiles/ddns-updater.nix
@@ -0,0 +1,43 @@
+{ config, lib, pkgs, machine, data, ... }:
+
+let
+  conf = pkgs.writeTextFile {
+    name = "conf";
+    text = builtins.toJSON {
+      settings = [
+        {
+          provider = "porkbun";
+          domain = "l3mon4.de";
+          host = "@";
+          api_key = "$PORKBUN_API_KEY";
+          secret_api_key = "$PORKBUN_SECRET_API_KEY";
+          ip_version = "ipv4";
+          ttl = 60;
+        }
+      ];
+    };
+  };
+  runtime_conf_file = "/run/ddns-updater.conf";
+in {
+  system.activationScripts = {
+    ddns-updater = {
+      text = ''
+
+        (umask 0077 && \
+        PORKBUN_API_KEY=$(cat /var/secrets/porkbun_api_key) \
+        PORKBUN_SECRET_API_KEY=$(cat /var/secrets/porkbun_secret_api_key) \
+        ${pkgs.envsubst}/bin/envsubst -i ${conf} -o ${runtime_conf_file})
+      '';
+    };
+  };
+
+  services.ddns-updater = {
+    enable = true;
+    environment = {
+      SERVER_ENABLED="no";
+      CONFIG_FILEPATH = "%d/conf";
+    };
+  };
+  systemd.services.ddns-updater.serviceConfig.LoadCredential = "conf:${runtime_conf_file}";
+}
+
