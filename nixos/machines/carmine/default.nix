da8c06bb2581f9091ec7354474239c6977202129
Simon Katz <simon@l3mon4.de>
2025-02-07 23:29:07 +0100

    nixos: add misc,media-mounts for carmine.

--- nixos/machines/carmine/default.nix
+++ nixos/machines/carmine/default.nix
@@ -7,5 +7,16 @@
     ./hardware-configuration.nix
   ];
 
+  fileSystems."/srv/misc" = {
+    device = "indigo:/misc";
+    fsType = "nfs";
+    options = [ "nfsvers=4.2" "rw" "acl" "fsc" ];
+  };
+  fileSystems."/srv/media" = {
+    device = "indigo:/media";
+    fsType = "nfs";
+    options = [ "nfsvers=4.2" "rw" "acl" "fsc" ];
+  };
+
   boot.loader.systemd-boot.enable = true;
 }
