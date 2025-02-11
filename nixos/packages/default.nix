9c6a770388e5dff566b93b1204a452814c019040
Simon Katz <simon@l3mon4.de>
2025-02-11 16:31:52 +0100

    nixos: replace netns-exec bash script with suid-binary.
    Don't have to use sudo, and can be certain that only the
    network-namespace is changed.

--- nixos/packages/default.nix
+++ nixos/packages/default.nix
@@ -1,7 +1,6 @@
 final: prev: {
   # special scope for my own packages.
   l3mon = {
-    netns-exec = prev.callPackage ./netns-exec.nix { };
     iosevka = prev.callPackage ./iosevka.nix { };
     k-sway = prev.luajitPackages.callPackage ./k-sway.nix { };
     k-stream = prev.luajitPackages.callPackage ./k-stream.nix { };
