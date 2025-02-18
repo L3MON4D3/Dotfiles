29d286309f9dd1dd75161e174d0a366037d11da9
Simon Katz <simon@l3mon4.de>
2025-02-18 21:32:47 +0100

    nixos: refactor wildhorn-connection into own module.

---
+++ nixos/profiles/unibonn.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, machine, data, ... }:
+
+{
+  services.openvpn.servers = {
+    unibonn = {config = "config /var/secrets/unibonn.conf";};
+  };
+  # log in once with root!
+  fileSystems."/mnt/wildhorn" = {
+    device = "katz@wildhorn.cs.uni-bonn.de:/";
+    fsType = "sshfs";
+    # only mount on-demand.
+    options = [ "noauto" "nodev" "noatime" "allow_other" "ro" "IdentityFile=/var/secrets/id_rsa" "uid=1000" "gid=1000" "x-systemd-requires=openvpn-unibonn.service" ];
+  };
+}
