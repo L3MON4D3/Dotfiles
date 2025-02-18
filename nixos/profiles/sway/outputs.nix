bc488464880ad128716c5c2cab684c0dcc3de913
Simon Katz <simon@l3mon4.de>
2025-02-18 22:33:13 +0100

    nixos: add config for toggling dpms and suspend.

--- nixos/profiles/sway/outputs.nix
+++ nixos/profiles/sway/outputs.nix
@@ -1,6 +1,24 @@
 { config, lib, pkgs, machine, data, ... }:
 
-{
+let
+  dpms_toggle = pkgs.writeShellApplication {
+    name = "dpms_toggle";
+    runtimeInputs = with pkgs; [
+      jq
+      sway
+    ];
+    text = ''
+      output=$1
+      if swaymsg -t get_outputs | jq -e ".[] | select(.name==\"''${output}\") | .dpms"; then
+        echo swaymsg output "''${output}" dpms off
+        swaymsg output "''${output}" dpms off
+      else
+        echo swaymsg output "''${output}" dpms on
+        swaymsg output "''${output}" dpms on
+      fi
+    '';
+  };
+in {
   wayland.windowManager.sway.extraConfig = ''
     output "BNQ BenQ RL2455 H5G01051SL0" transform 90 resolution 1920x1080@60.000Hz position 2560,0
     output "BNQ BenQ RL2455 H5G01051SL0" subpixel vbgr
@@ -11,5 +29,10 @@
     output "Red Hat, Inc. QEMU Monitor Unknown" transform 0 mode 1280x800@74.994Hz position 0,0
 
     output "Unknown Unknown Unknown" transform 0 mode 1600x1200@60.000Hz position 0,0
+
+    mode "power" {
+      bindsym --no-repeat d output DP-1 dpms toggle; mode "default"
+      bindsym --no-repeat h output HDMI-A-1 dpms toggle; mode "default"
+    }
   '';
 }
