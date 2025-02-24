c5d177eb4906c33937797d1e3108659405debea8
Simon Katz <simon@l3mon4.de>
2025-02-25 00:48:18 +0100

    nixos: fix xdg-portal config.

--- nixos/profiles/sway/default.nix
+++ nixos/profiles/sway/default.nix
@@ -132,7 +132,7 @@ in {
     };
   };
 
- xdg.portal = {
+  xdg.portal = {
     enable = true;
     config = {
       common = {
@@ -145,6 +145,11 @@ in {
       chooser_type = "simple";
       chooser_cmd = "${pkgs.slurp}/bin/slurp -f %o -or";
     };
+    xdgOpenUsePortal = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+      pkgs.xdg-desktop-portal-gnome
+    ];
   };
 
   home-manager.sharedModules = [
@@ -240,27 +245,57 @@ in {
           };
         };
 
-        # xdg.portal = {
-          # enable = true;
-          # config.common = {
-            # default = [
-              # "wlr"
-              # "gtk"
-              # "gnome"
-            # ];
-            # "org.freedesktop.impl.portal.ScreenCast" = "wlr";
-            # "org.freedesktop.impl.portal.Screenshot" = "wlr";
-            # "org.freedesktop.impl.portal.FileChooser" = "gtk";
-            # "org.freedesktop.impl.portal.AppChooser" = "gtk";
-            # "org.freedesktop.impl.portal.Print" = "gtk";
-            # "org.freedesktop.impl.portal.Notification" = "gtk";
-          # };
-          # extraPortals = with pkgs; [
-            # xdg-desktop-portal-wlr
-            # xdg-desktop-portal-gtk
-            # xdg-desktop-portal-gnome
-          # ];
-        # };
+        xdg.portal = {
+          enable = true;
+          config.common = {
+            default = [
+              "wlr"
+              "gtk"
+              "gnome"
+            ];
+            # https://github.com/NixOS/nixpkgs/issues/262286
+            "org.freedesktop.impl.portal.Inhibit" = "none";
+
+            "org.freedesktop.impl.portal.ScreenCast" = "wlr";
+            "org.freedesktop.impl.portal.Screenshot" = "wlr";
+
+            # GTK interfaces
+            "org.freedesktop.impl.portal.FileChooser" = "gtk";
+            "org.freedesktop.impl.portal.AppChooser" = "gtk";
+            "org.freedesktop.impl.portal.Print" = "gtk";
+            "org.freedesktop.impl.portal.Notification" = "gtk";
+            # "org.freedesktop.impl.portal.Inhibit" = "gtk";
+            "org.freedesktop.impl.portal.Access" = "gtk";
+            "org.freedesktop.impl.portal.Account" = "gtk";
+            "org.freedesktop.impl.portal.Email" = "gtk";
+            "org.freedesktop.impl.portal.DynamicLauncher" = "gtk";
+            "org.freedesktop.impl.portal.Lockdown" = "gtk";
+            "org.freedesktop.impl.portal.Settings" = "gtk";
+            "org.freedesktop.impl.portal.Wallpaper" = "gtk";
+
+            # "org.freedesktop.impl.portal.Access" = "gnome";
+            # "org.freedesktop.impl.portal.Account" = "gnome";
+            # "org.freedesktop.impl.portal.AppChooser" = "gnome";
+            "org.freedesktop.impl.portal.Background" = "gnome";
+            "org.freedesktop.impl.portal.Clipboard" = "gnome";
+            # "org.freedesktop.impl.portal.DynamicLauncher" = "gnome";
+            # "org.freedesktop.impl.portal.FileChooser" = "gnome";
+            "org.freedesktop.impl.portal.InputCapture" = "gnome";
+            # "org.freedesktop.impl.portal.Lockdown" = "gnome";
+            # "org.freedesktop.impl.portal.Notification" = "gnome";
+            # "org.freedesktop.impl.portal.Print" = "gnome";
+            "org.freedesktop.impl.portal.RemoteDesktop" = "gnome";
+            # "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+            # "org.freedesktop.impl.portal.Screenshot" = "gnome";
+            # "org.freedesktop.impl.portal.Settings" = "gnome";
+            # "org.freedesktop.impl.portal.Wallpaper" = "gnome";
+          };
+          extraPortals = with pkgs; [
+            xdg-desktop-portal-wlr
+            xdg-desktop-portal-gtk
+            xdg-desktop-portal-gnome
+          ];
+        };
 
         # important!!! needs wrapperFeatures=gtk in sway.
         gtk = {
