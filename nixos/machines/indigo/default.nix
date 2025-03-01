72f871dfdcf4f0206a6cceeadc13ec4cad7d4cfa
Simon Katz <simon@l3mon4.de>
2025-03-01 13:07:48 +0100

    nixos: enable nvidia drivers.
    jellyfin hardware transcoding!!

--- nixos/machines/indigo/default.nix
+++ nixos/machines/indigo/default.nix
@@ -255,4 +255,19 @@
   environment.shellAliases = {
     re = ''sudo ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake "/home/simon/projects/dotfiles/nixos#indigo"'';
   };
+
+  # https://nixos.wiki/wiki/Nvidia
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    # load even if no graphical session.
+    nvidiaPersistenced = true;
+    # newer open driver only supports <= turing(20xx)
+    open = false;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+  # for mesa, which provides hardware acceleration for video encoding (I think).
+  hardware.graphics.enable = true;
+  # also necessary for transcoding.
+  services.xserver.videoDrivers = [ "nvidia" ];
 }
