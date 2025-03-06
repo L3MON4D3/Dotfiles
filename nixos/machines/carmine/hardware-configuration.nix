e16bf035db29ce2ac2dc5c5018397deca161b18d
Simon Katz <simon@l3mon4.de>
2025-03-06 20:40:18 +0100

    nixos: enable swap-partition on carmine.

--- nixos/machines/carmine/hardware-configuration.nix
+++ nixos/machines/carmine/hardware-configuration.nix
@@ -24,7 +24,11 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {
+      device = "/dev/disk/by-label/SWAP";
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
