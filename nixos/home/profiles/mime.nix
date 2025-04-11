fa9f8a45d9e6897b883578b3748a57b43f99bcf9
Simon Katz <simon@l3mon4.de>
2025-04-11 12:41:36 +0200

    nixos: fix mimeapps-behaviour.
    Importing PATH is suggested here, https://discourse.nixos.org/t/xdg-portals-all-broken/48308/12.
    qutebrowser was not used because the .desktop-file name was incorrect.

--- nixos/home/profiles/mime.nix
+++ nixos/home/profiles/mime.nix
@@ -1,25 +1,33 @@
 { config, lib, pkgs, machine, data, ... }:
 
 let 
-  browser = ["qutebrowser.desktop"];
+  qutebrowser = ["org.qutebrowser.qutebrowser.desktop"];
+  zathura = ["org.pwmt.zathura-pdf-mupdf.desktop"];
+  thunderbird = ["userapp-Thunderbird-D15E22.desktop"];
+
   associations = {
     # "inode/directory" = [ "foot.desktop" ];
-    "text/html" = browser;
-    "x-scheme-handler/http" = browser;
-    "x-scheme-handler/https" = browser;
-    "x-scheme-handler/ftp" = browser;
-    "x-scheme-handler/chrome" = browser;
-    "x-scheme-handler/about" = browser;
-    "x-scheme-handler/unknown" = browser;
-    "application/x-extension-htm" = browser;
-    "application/x-extension-html" = browser;
-    "application/x-extension-shtml" = browser;
-    "application/xhtml+xml" = browser;
-    "application/x-extension-xhtml" = browser;
-    "application/x-extension-xht" = browser;
-    "x-scheme-handler/mailto" = ["userapp-Thunderbird-D15E22.desktop"];
-    "message/rfc822" = ["userapp-Thunderbird-D15E22.desktop"];
-    "x-scheme-handler/mid" = ["userapp-Thunderbird-D15E22.desktop"];
+    "text/html" = qutebrowser;
+
+    "x-scheme-handler/http" = qutebrowser;
+    "x-scheme-handler/https" = qutebrowser;
+    "x-scheme-handler/about" = qutebrowser;
+    "x-scheme-handler/unknown" = qutebrowser;
+    "x-scheme-handler/ftp" = qutebrowser;
+    "x-scheme-handler/chrome" = qutebrowser;
+
+    "application/x-extension-htm" = qutebrowser;
+    "application/x-extension-html" = qutebrowser;
+    "application/x-extension-shtml" = qutebrowser;
+    "application/xhtml+xml" = qutebrowser;
+    "application/x-extension-xhtml" = qutebrowser;
+    "application/x-extension-xht" = qutebrowser;
+
+    "application/pdf" = zathura;
+
+    "x-scheme-handler/mailto" = thunderbird;
+    "message/rfc822" = thunderbird;
+    "x-scheme-handler/mid" = thunderbird;
   };
 in {
   xdg.mime.enable = true;
@@ -28,5 +36,4 @@ in {
     associations.added = associations;
     defaultApplications = associations;
   };
-
 }
