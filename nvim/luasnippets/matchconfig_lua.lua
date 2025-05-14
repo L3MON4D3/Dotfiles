041ae6b81a8b6cebefed1a39015dbd17591622ec
Simon Katz <simon@l3mon4.de>
2025-05-14 15:57:15 +0200

    nvim: add snippet for temp-logging.

--- nvim/luasnippets/matchconfig_lua.lua
+++ nvim/luasnippets/matchconfig_lua.lua
@@ -1,1 +1,10 @@
 parse_add_auto("MC", "Matchconfig.")
+s_add("lvi", fmt([[
+	require("matchconfig.util.log").new("temp").error({})
+]], {
+	c(1, {
+		parse(nil, [["$1"]]),
+		parse(nil, [["$1" .. vim.inspect({$1})]]),
+		t"debug.traceback()",
+	})
+}))
