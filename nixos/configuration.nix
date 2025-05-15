4b98d9c6904c3974dbb702c0a4d4771dd12f6694
Simon Katz <simon@l3mon4.de>
2025-05-15 20:01:37 +0200

    nixos: make myself a trusted user.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -185,6 +185,9 @@
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
+      trusted-users = [
+        "simon"
+      ];
     };
     registry = {
      vorlagen = {
