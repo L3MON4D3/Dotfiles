a87785f0c0d56a5c560fe135cd694f72ab2b6ac7
Simon Katz <simon@l3mon4.de>
2025-05-20 09:44:18 +0200

    nixos: update neovim-nightly.
    Seems like that hl-bug/change is fixed/undone! :)

--- nvim/generated/rtp_base.lua
+++ nvim/generated/rtp_base.lua
@@ -1,1 +1,1 @@
-return {"/nix/store/a5ik5lgdzxs8ka0401cjcqhv0i57skc1-neovim-unwrapped-nightly/share/nvim/runtime"}
+return {"/nix/store/3sdfnq6f0mq5lp0gk4gssm8cf020pb74-neovim-unwrapped-nightly/share/nvim/runtime"}
