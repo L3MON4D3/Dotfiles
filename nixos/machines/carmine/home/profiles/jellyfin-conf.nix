ba22237b9d451aba38101f7bd1bd8ecba11467b1
Simon Katz <simon@l3mon4.de>
2025-05-31 23:01:55 +0200

    nixos: update jellyfin-mpv-shim credentials.

--- nixos/machines/carmine/home/profiles/jellyfin-conf.nix
+++ nixos/machines/carmine/home/profiles/jellyfin-conf.nix
@@ -3,6 +3,6 @@
 {
   # maybe have to update this?
   xdg.configFile."jellyfin-mpv-shim/cred.json".text = ''
-    [{"address": "http://jellyfin.internal:80", "Name": "indigo", "Id": "19115077000e43dca1172812a4f96c59", "Version": "10.10.3", "DateLastAccessed": "2025-02-14T01:10:13Z", "UserId": "db65cdf9d37b4d7cb0fdfe39058c5657", "AccessToken": "5d257143d72e4e5aa5a77f111075f596", "Users": [{"Id": "db65cdf9d37b4d7cb0fdfe39058c5657", "IsSignedInOffline": true}], "uuid": "684f0a2a-1753-4bc7-aee4-62ca0553a5bd", "username": "simon", "connected": true}]
+    [{"address": "http://jellyfin.internal", "Name": "indigo", "Id": "19115077000e43dca1172812a4f96c59", "Version": "10.10.7", "DateLastAccessed": "2025-05-31T23:00:28Z", "UserId": "db65cdf9d37b4d7cb0fdfe39058c5657", "AccessToken": "6f98461806084700b59e58475c2cd698", "Users": [{"Id": "db65cdf9d37b4d7cb0fdfe39058c5657", "IsSignedInOffline": true}], "uuid": "ef4258af-90d9-4f29-9219-f604c9ab6ba7", "username": "simon", "connected": true}]
   '';
 }
