056f13737b0043dcdb87a21e6267df6c3dba36b2
Simon Katz <simon@l3mon4.de>
2025-06-12 22:17:58 +0200

    nixos: make sway-io slightly more DRY.

--- nixos/machines/carmine/home/profiles/sway-io.nix
+++ nixos/machines/carmine/home/profiles/sway-io.nix
@@ -1,6 +1,9 @@
 { config, lib, pkgs, machine, data, ... }:
 
-{
+let
+  primary = "DP-1";
+  secondary = "HDMI-A-1";
+in {
   wayland.windowManager.sway.extraConfig = ''
     input type:pointer {
       pointer_accel -1
@@ -21,21 +24,21 @@
         xkb_variant "altgr-intl"
     }
 
-    output "BNQ BenQ RL2455 H5G01051SL0" transform 90 resolution 1920x1080@60.000Hz position 2560,0
-    output "BNQ BenQ RL2455 H5G01051SL0" subpixel vbgr
+    output "${secondary}" transform 90 resolution 1920x1080@60.000Hz position 2560,0
+    output "${secondary}" subpixel vbgr
 
-    output "Microstep MSI MAG271CQR 0x30303441" transform 0 mode 2560x1440@143.999Hz position 0,0
-    output "Microstep MSI MAG271CQR 0x30303441" subpixel rgb
+    output "${primary}" transform 0 mode 2560x1440@120.066Hz position 0,0
+    output "${primary}" subpixel rgb
 
     output "Red Hat, Inc. QEMU Monitor Unknown" transform 0 mode 1280x800@74.994Hz position 0,0
 
     output "Unknown Unknown Unknown" transform 0 mode 1600x1200@60.000Hz position 0,0
 
     mode "power" {
-      bindsym --no-repeat d output DP-1 dpms toggle; mode "default"
-      bindsym --no-repeat h output HDMI-A-1 dpms toggle; mode "default"
+      bindsym --no-repeat d output "${primary}" dpms toggle; mode "default"
+      bindsym --no-repeat h output "${secondary}" dpms toggle; mode "default"
 
-      bindsym --no-repeat b output DP-1 dpms toggle; output HDMI-A-1 dpms toggle; mode "default"
+      bindsym --no-repeat b output "${primary}" dpms toggle; output "${secondary}" dpms toggle; mode "default"
     }
   '';
 }
