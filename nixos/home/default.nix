d00b42c82299bdd68cfe0efb8eb52340714f542d
Simon Katz <simon@l3mon4.de>
2025-07-07 15:05:15 +0200

    nixos: pinentryPackage -> pinentry.package.

--- nixos/home/default.nix
+++ nixos/home/default.nix
@@ -166,7 +166,7 @@
 
   services.gpg-agent = {
     enable = true;
-    pinentryPackage = pkgs.pinentry-gnome3;
+    pinentry.package = pkgs.pinentry-gnome3;
   };
 
   xdg.configFile."nixpkgs/config.nix".text = ''
