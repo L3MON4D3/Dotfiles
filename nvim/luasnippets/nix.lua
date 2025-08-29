1c82492a1623669ef8bfb9eada435a53cb2d8cb2
Simon Katz <simon@l3mon4.de>
2025-08-29 12:08:44 +0200

    nvim: add snippet for writeShellApplication.

--- nvim/luasnippets/nix.lua
+++ nvim/luasnippets/nix.lua
@@ -6,3 +6,22 @@ parse_add("prof", [[
 		$1
 	}
 ]])
+s_add("wsa", fmta([[
+	<pkgs>.writeShellApplication {
+		name = "<name>";<rti>
+		text = ''
+			<txt>
+		'';
+	}
+]], {
+	pkgs = i(1, "pkgs", {key = "pkgs"}),
+	name = i(2, "name"),
+	rti = c(3, {
+		t"",
+		fmta("\n\truntimeInputs = with <pkgsrep>; [<pkgs>];", {
+			pkgsrep = l(l._1, {k"pkgs"}),
+			pkgs = i(1)
+		}, {trim_empty = false, dedent = false}),
+	}),
+	txt = i(4)
+}))
