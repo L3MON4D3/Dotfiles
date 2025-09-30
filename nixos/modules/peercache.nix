528763f41126bfbe80abc26e96ca55fb9a810e53
Simon Katz <simon@l3mon4.de>
2025-09-30 17:11:46 +0200

    nixos: handle peercache-keys via secgen, update pubkey.

--- nixos/modules/peercache.nix
+++ nixos/modules/peercache.nix
@@ -24,14 +24,41 @@ with lib;
         trusted-public-keys = lib.attrsets.foldlAttrs (acc: name: os:
           if os.config.l3mon.peercache.push && name != machine then
             # we only use this key for signing.
-            acc ++ ["cache.${name}.internal:CA2Hg9Xq3wNRTOU/Pombi0CLc2aemwlyPw/o34zDrKA="]
+            acc ++ ["cache.${name}.internal:dmRV8DJW4a9BBGtzXO2bYAfZAU5l7utBg4n4fTkfSRE="]
           else acc) [] self.outputs.nixosConfigurations;
       };
     })
     (mkIf cfg.push {
+
+      l3mon.secgen.secrets.peercache = rec {
+        backup_relfiles = [];
+        gen = pkgs.writeShellApplication {
+          name = "gen";
+          text =
+          # bash 
+          ''
+            tmpkey=$(mktemp)
+            tmppubkey=$(mktemp)
+
+            nix-store --generate-binary-cache-key "" "$tmpkey" "$tmppubkey"
+
+            KEY=$(cat "$tmpkey")
+            PUBKEY=$(cat "$tmppubkey")
+
+            echo "Update pubkey to $PUBKEY in peercache.nix"
+
+            rm "$tmpkey"
+            rm "$tmppubkey"
+          '' +
+            lib.attrsets.foldlAttrs (acc: name: _: acc + ''
+              echo -n "cache.${name}.internal$KEY" > /var/secrets/nix-serve-key-${name}
+            '') "" self.outputs.nixosConfigurations;
+        };
+      } // lib.attrsets.concatMapAttrs (name: os: {"key-${name}" = "/var/secrets/nix-serve-key-${name}";}) self.outputs.nixosConfigurations;
+
       services.nix-serve = {
         enable = true;
-        secretKeyFile = l3lib.secret "cache-key-${machine}.pem";
+        secretKeyFile = config.l3mon.secgen.secrets.peercache."key-${machine}";
         port = data.ports.nix-serve;
       };
 
