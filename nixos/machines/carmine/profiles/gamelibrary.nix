e91ba7ddf50f37b1dc66f7ae0afaacc08cf28c5f
Simon Katz <simon@l3mon4.de>
2025-11-17 23:51:18 +0100

    nixos: update.

--- nixos/machines/carmine/profiles/gamelibrary.nix
+++ nixos/machines/carmine/profiles/gamelibrary.nix
@@ -33,7 +33,33 @@
   environment.systemPackages = [
     pkgs.blueman
 
-    pkgs-yuzu.yuzu
+    (pkgs.callPackage ({
+      fetchurl,
+      appimageTools,
+      ...
+    }: let
+      pname = "yuzu";
+      version = "EA-4176";
+
+      src = fetchurl {
+        url = "https://archive.org/download/citra-qt-and-yuzu-EA/Linux-Yuzu-EA-4176.AppImage";
+        sha256 = "sha256-bUTVL8br2POy5HB1FszlNQNChdRWcwIlG6/RCceXIlg=";
+      };
+
+      appimage-contents = appimageTools.extract {
+        inherit pname version src;
+      };
+
+      desktop-file-name = "org.yuzu_emu.yuzu";
+    in
+      appimageTools.wrapAppImage {
+        inherit pname version;
+        src = appimage-contents;
+        extraInstallCommands = ''
+          install -m 444 -D ${appimage-contents}/${desktop-file-name}.desktop -t $out/share/applications
+          cp -r ${appimage-contents}/usr/share/icons $out/share
+        '';
+      }) {})
     pkgs-suyu.suyu
     pkgs-unstable.ryubing
 
@@ -92,6 +118,7 @@
           ln -s /home/simon/.config/Ryujinx ryujinx
           ln -s /home/simon/.local/share/dolphin-emu dolphin
           ln -s /home/simon/.local/share/suyu suyu
+          ln -s /home/simon/.local/share/yuzu yuzu
           ln -s /home/simon/games games
           ln -s /home/simon/.local/share/lutris lutris
           ln -s /home/simon/.local/state/melonDS melonDS
