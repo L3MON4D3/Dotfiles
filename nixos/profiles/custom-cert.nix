6af564925d49853fab28ae949c0f879d95ea618f
Simon Katz <simon@l3mon4.de>
2026-01-17 01:43:01 +0100

    nixos: provide certs in systemd user services.

--- nixos/profiles/custom-cert.nix
+++ nixos/profiles/custom-cert.nix
@@ -1,13 +1,23 @@
 { config, lib, pkgs, machine, data, ... }:
 
 let
-  envs = {
+  vars = {
     # for python.
     REQUESTS_CA_BUNDLE = config.security.pki.caBundle;
   };
 in {
   security.pki.certificateFiles = [ config.l3mon.secgen.secrets.caddy_ca_root.cert ];
 
-  environment.variables = envs;
-  systemd.globalEnvironment = envs;
+  environment.variables = vars;
+  systemd.globalEnvironment = vars;
+
+  home-manager.sharedModules = [
+    ({
+      systemd.user.sessionVariables = vars;
+      xdg.configFile."systemd/user/service.d/cert_env.conf".text = ''
+        [Service]
+        Environment=REQUESTS_CA_BUNDLE=${config.security.pki.caBundle}
+      '';
+    })
+  ];
 }
