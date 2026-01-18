99b9aa43d656c83da8d22d70d9a57a9a316afb98
Simon Katz <simon@l3mon4.de>
2026-01-18 10:18:47 +0100

    nixos: back up silverbullet.

--- nixos/profiles/silverbullet.nix
+++ nixos/profiles/silverbullet.nix
@@ -13,5 +13,20 @@
   };
   users.groups.silverbullet.gid = data.ids.silverbullet;
 
-  l3mon.services.defs.silverbullet.cfg = data.ports.silverbullet;
+  l3mon.services.defs.sb.cfg = data.ports.silverbullet;
+
+  l3mon.restic.extraGroups = [ "silverbullet" ];
+  l3mon.restic.specs.silverbullet = {
+    backupDaily = {
+      text = ''
+        cd /var/lib/silverbullet/
+        restic backup --tag=silverbullet --skip-if-unchanged=true -- ./*
+      '';
+    };
+    forget = {
+      text = ''
+        restic forget --tag=silverbullet --group-by=tag --keep-daily=7 --keep-monthly=12
+      '';
+    };
+  };
 }
