781b1d1f908a36b96e3c6c8455716b61db00dce3
Simon Katz <simon@l3mon4.de>
2026-02-06 23:06:39 +0100

    nixos: update mullvad.

--- nixos/data/generated/wg-mullvad_de.nix
+++ nixos/data/generated/wg-mullvad_de.nix
@@ -1,8 +1,8 @@
 
 {
-  pubkey = "+nqH8e7xvI4+sfAt2r0Y8C1e8ZfiXDqS0b3wHm00ilU=";
-  address = "10.74.224.236";
+  pubkey = "kAZ5Jz7xCx3WsHvV2kGsF3/kBwaJyRSn2+esddgN0xQ=";
+  address = "10.74.201.92";
   dns = "10.64.0.1";
-  remote_pubkey =  "Sttn2cr14dvIcCrE8qdlRGHXriqvTyvQWC7dzujH/iM=";
-  remote_endpoint =  "146.70.117.2:51820";
+  remote_pubkey =  "tzYLWgBdwrbbBCXYHRSoYIho4dHtrm+8bdONU1I8xzc=";
+  remote_endpoint =  "185.209.196.74:51820";
 }
